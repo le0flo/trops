8fcbfbfcf94a53548aabadc8d8cf68b62e85f054
Leonardo <128467278+le0flo@users.noreply.github.com>
2024-04-03 17:08:27 +0200

    H0-2 | Fix repo

--- src/main/java/it/avbo/tps/gruppotre/hoolibo/api/ConnectionFactory.java
+++ src/main/java/it/avbo/tps/gruppotre/hoolibo/api/ConnectionFactory.java
@@ -1,4 +1,34 @@
 package it.avbo.tps.gruppotre.hoolibo.api;
 
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+
+    private static MariaDbDataSource ds;
+
+    private static void createConnection() {
+        try {
+            ds = new MariaDbDataSource();
+            ds.setUrl("jdbc:mariadb://localhost:3306/hoolibo");
+            ds.setUser("root");
+            ds.setPassword("athena");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection() {
+        if (ds == null) {
+            createConnection();
+        }
+
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            return null;
+        }
+    }
 }
--- src/main/java/it/avbo/tps/gruppotre/hoolibo/api/managers/SessionsManager.java
+++ src/main/java/it/avbo/tps/gruppotre/hoolibo/api/managers/SessionsManager.java
@@ -1,2 +1,34 @@
-package it.avbo.tps.gruppotre.hoolibo.api.managers;public class SessionsManager {
+package it.avbo.tps.gruppotre.hoolibo.api.managers;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class SessionsManager {
+
+    private static SessionsManager instance;
+
+    private HashMap<UUID, String> activeSessions = new HashMap<>();
+
+    public String retrieveAccount(UUID session) {
+        return activeSessions.get(session);
+    }
+
+    public UUID generateSession(String email) {
+        UUID uuid = UUID.randomUUID();
+
+        if (activeSessions.get(uuid) != null) {
+            return generateSession(email);
+        } else {
+            activeSessions.put(uuid, email);
+            return uuid;
+        }
+    }
+
+    public static SessionsManager getInstance() {
+        if (instance == null) {
+            instance = new SessionsManager();
+        }
+
+        return instance;
+    }
 }
