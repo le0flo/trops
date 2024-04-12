9182a209b4bc4a92200a197895e5aa4d2a270a52
Leonardo <128467278+le0flo@users.noreply.github.com>
2024-04-12 18:01:56 +0200

    H0-3 | Fix status code response

--- src/main/java/it/avbo/tps/hoolibo/trops/api/accounts/endpoints/DeactivateSession.java
+++ src/main/java/it/avbo/tps/hoolibo/trops/api/accounts/endpoints/DeactivateSession.java
@@ -33,7 +33,7 @@ public class DeactivateSession extends HttpServlet {
 
             if (account == null || deactivatedSessionAccount == null) {
                 writer.println(response.errorInvalidSessionUUID());
-                resp.sendError(500);
+                resp.setStatus(500);
             } else if (account.equals(deactivatedSessionAccount)) {
                 UUID unregisteredSession = sessionsManager.unregisterSession(deactivatedSession);
 
@@ -43,11 +43,11 @@ public class DeactivateSession extends HttpServlet {
                 writer.println(response.success(values));
             } else {
                 writer.println(response.errorSessionNotOwned());
-                resp.sendError(500);
+                resp.setStatus(500);
             }
         } catch (IllegalArgumentException e) {
             response.handleException(e, this.getClass());
-            resp.sendError(500);
+            resp.setStatus(500);
         }
     }
 }
