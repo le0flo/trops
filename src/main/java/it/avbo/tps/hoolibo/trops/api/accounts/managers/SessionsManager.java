0f38899ffddc0ee14467a0d3c1136bbdc74aefab
Leonardo <leonflorenzano@protonmail.com>
2024-04-17 11:19:16 +0200

    H0-32 | implementazione redis

--- src/main/java/it/avbo/tps/hoolibo/trops/api/accounts/managers/SessionsManager.java
+++ src/main/java/it/avbo/tps/hoolibo/trops/api/accounts/managers/SessionsManager.java
@@ -1,5 +1,8 @@
 package it.avbo.tps.hoolibo.trops.api.accounts.managers;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -9,23 +12,35 @@ public class SessionsManager {
 
     private HashMap<UUID, String> activeSessions = new HashMap<>();
 
+    private JedisPool pool;
+
+    public SessionsManager() {
+        pool = new JedisPool(System.getenv("REDIS_HOST"), 6379);
+    }
+
     public UUID generateSession(String email) {
         UUID uuid = UUID.randomUUID();
 
         if (activeSessions.get(uuid) != null) {
             return generateSession(email);
         } else {
-            activeSessions.put(uuid, email);
+            try (Jedis jedis = pool.getResource()) {
+                jedis.set(uuid.toString(), email);
+            }
             return uuid;
         }
     }
 
     public String retrieveAccount(UUID session) {
-        return activeSessions.get(session);
+        try (Jedis jedis = pool.getResource()) {
+            return jedis.get(session.toString());
+        }
     }
 
     public UUID unregisterSession(UUID session) {
-        activeSessions.remove(session);
+        try (Jedis jedis = pool.getResource()) {
+            jedis.del(session.toString());
+        }
         return session;
     }
 
