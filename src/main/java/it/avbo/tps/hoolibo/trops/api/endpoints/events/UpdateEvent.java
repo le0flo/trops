e411a537e919384580ea0e36b9f14adccf4ab8ab
Christian Degli Esposti <chri.degli@gmail.com>
2024-04-24 17:38:09 +0200

    H0-3 | Aggiornata la lista di endpoint e creati templates

---
+++ src/main/java/it/avbo/tps/hoolibo/trops/api/endpoints/events/UpdateEvent.java
@@ -0,0 +1,8 @@
+package it.avbo.tps.hoolibo.trops.api.endpoints.events;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/events/update")
+public class UpdateEvent extends HttpServlet {
+}
---
+++ src/main/java/it/avbo/tps/hoolibo/trops/api/endpoints/places/CreatePlace.java
@@ -0,0 +1,8 @@
+package it.avbo.tps.hoolibo.trops.api.endpoints.places;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/places/create")
+public class CreatePlace extends HttpServlet {
+}
